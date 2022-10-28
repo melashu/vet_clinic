d15d182fe4f4d95949a969345740e579625e466f
melashu <meshu102@outlook.com>
2022-10-28 08:41:26 +0300

    Add data to species table

--- data.sql
+++ data.sql
@@ -13,6 +13,7 @@ INSERT INTO animals (name,neutered,escape_attempts,date_of_birth,weight_kg) VALU
 INSERT INTO animals (name,neutered,escape_attempts,date_of_birth,weight_kg) VALUES ('Ditto',TRUE,4,'2022-05-14',22);
 
 INSERT INTO owners (full_name,age) VALUES ('Sam Smith',34),('Jennifer Orwell',19),('Bob',45),('Melody Pond',77),('Dean Winchester',14),('Jodie Whittake',38);
+INSERT INTO species (name) VALUES ('Pokemon'),('Digimon');
 
 UPDATE animals SET owner_id=1 where name='Agumon';
 UPDATE animals SET owner_id=2 where name IN ('Gabumon','Pikachu');
