2d82ac961470a5a2427189f794cb405ce45acb4e
melashu <meshu102@outlook.com>
2022-10-29 09:21:32 +0300

    Insert data into vets,specializations, and visits table

--- data.sql
+++ data.sql
@@ -23,3 +23,39 @@ UPDATE animals SET owner_id=5 where name IN ('Angemon','Boarmon');
 
 UPDATE animals SET species_id=2 where name LIKE '%mon';
 UPDATE animals SET species_id=1 where name NOT LIKE '%mon';
+
+
+INSERT INTO vets (name,age,date_of_graduation) 
+VALUES ('William Tatcher',45,'2000-04-23'),
+('Maisy Smith',26,'2019-01-17'),
+('Stephanie Mendez',64,'1981-05-04'),
+('Jack Harkness',38,'2008-06-08');
+
+INSERT INTO specializations (vets_id,species_id)
+ VALUES (1,1),
+ (3,1),
+ (3,2),
+ (4,2);
+INSERT INTO VISITS(animals_id,vets_id,visist_date)
+ VALUES (13,3,'2020-07-22'),
+ (21,4,'2021-02-02'),
+ (22,2,'2020-01-05'),
+ (22,2,'2020-03-08'),
+ (22,2,'2020-06-14'),
+ (4,3,'2021-05-04'),
+ (14,4,'2021-02-24'),
+ (15,2,'2019-12-21'),
+ (15,1,'2020-08-10'),
+ (15,02,'2021-04-07'),
+ (16,3,'2019-09-29'),
+ (17,4,'2020-10-03'),
+ (17,4,'2020-11-04'),
+ (18,2,'2019-01-24'),
+ (18,2,'2019-05-15'),
+ (18,2,'2020-02-27'),
+ (18,2,'2020-08-03'),
+ (19,3,'2020-05-24'),
+ (19,1,'2021-01-11');
+
+
+ 
