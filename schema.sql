ef362fcb3b1eaf96f71b1187e665ee3a65d5f026
melashu <meshu102@outlook.com>
2022-10-29 09:20:34 +0300

    Create vets,specializations,specializations table

--- schema.sql
+++ schema.sql
@@ -25,4 +25,28 @@ ALTER TABLE animals DROP COLUMN species;
  ALTER TABLE animals ADD owner_id INT;
  ALTER TABLE animals ADD CONSTRAINT owner_constraint FOREIGN KEY (owner_id) REFERENCES owners (id);
 
+CREATE TABLE IF NOT EXISTS vets(
+id SERIAL,
+name VARCHAR(60),
+age INT,
+date_of_graduation date,
+PRIMARY KEY(id));
+
+CREATE TABLE IF NOT EXISTS specializations (
+species_id INT,
+vets_id INT,
+CONSTRAINT vets_fk FOREIGN KEY(vets_id) REFERENCES vets (id),
+CONSTRAINT species_fk FOREIGN KEY(species_id) REFERENCES species (id));
+
+CREATE TABLE IF NOT EXISTS visits (animals_id INT,
+vets_id INT,
+visist_date date,
+CONSTRAINT vets_fk
+ FOREIGN KEY(vets_id) 
+ REFERENCES vets (id),
+CONSTRAINT animals_fk 
+FOREIGN KEY(animals_id) 
+REFERENCES animals (id));
+
+
  
